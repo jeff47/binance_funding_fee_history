44a9918c42fc37ea251da6b156ab4725b790ee3e
Jeffrey Rice <jeff@bunkerhill>
2021-04-20 10:33:43 -0400

    Added options to modify EWMA decay factor ("span"). Added option for verbose output.

--- funding_history.py
+++ funding_history.py
@@ -7,16 +7,21 @@ import argparse
 import datetime as dt
 
 # This will obtain the funding fee history from Binance for all the perpetual crypto contracts, for the given time period.
-# Results will be saved in JSON and CSV format. It's easiest to load the CSV into Sheets and use a pivot table for analysis.
+# Results will be saved in CSV format.
 # All the data here is also available at https://www.binance.com/en/futures/funding-history/1, but the history is available
 # only for single tokens at a time.
 
 # How many days of history to get?  333 seems to be the max.
 all_args = argparse.ArgumentParser()
-all_args.add_argument("-d", "--days", required=True, help="Days of history to retrieve. 333 seems to be the maximum.")
+all_args.add_argument("-d", "--days", required=True, type=int, help="Days of history to retrieve. 333 seems to be the maximum.")
+all_args.add_argument("-s", "--span", default=9, required=False, type=int, help="Decay setting.  Higher numbers result in less weighting of recent rates. Defaults to 9 (96 hours).")
+all_args.add_argument("-v", "--verbose", action="store_true", help="Print symbols as they are fetched.")
+
 args = vars(all_args.parse_args())
 
-days = int(args['days'])
+days = args['days']
+span = args['span']
+verbose = args['verbose']
 
 # Query the Binance API to get all the symbols available for arbitrage
 page = requests.get('https://fapi.binance.com/fapi/v1/premiumIndex')
@@ -29,12 +34,17 @@ excludelist = ('ETHUSDT_210625', 'BTCUSDT_210625')
 all_agg_df = pd.DataFrame()
 for i in futures:
     if i["symbol"] in excludelist: continue
+    if verbose: print(i["symbol"])
     page = requests.get('https://fapi.binance.com/fapi/v1/fundingRate', params={'symbol':i["symbol"],'limit':days*3})
-    df = pd.DataFrame(json.loads(page.content))
-    df = df.astype({'fundingRate': 'float64'})
+    try:
+        df = pd.DataFrame(json.loads(page.content))
+        df = df.astype({'fundingRate': 'float64'})
+    except:
+        print("Could not parse data for symnbol " + i["symbol"])
+        continue
 
     # Calculate Exponential weighted moving average
-    df['EWMA'] = df['fundingRate'].ewm(span=21,adjust=False).mean()
+    df['EWMA'] = df['fundingRate'].ewm(span=span,adjust=False).mean()
     agg_df = df.groupby('symbol').fundingRate.agg(['mean', 'std', 'min', 'max', 'count'])
     agg_df.insert(0, 'EWMA', df['EWMA'].tail(1).to_string(index=False))
     all_agg_df = all_agg_df.append(agg_df)
